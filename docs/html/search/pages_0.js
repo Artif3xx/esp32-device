f4c013f1772866c327a357d45a6935430b2c3dc0
georg <Garbatronprivat@gmail.com>
2022-04-05 16:12:54 +0200

    edit DOXYFILE fir this project

--- docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['esp32_2ddevice_0',['ESP32-Device',['../md__read_me.html',1,'']]]
-];
---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief   simple test framework for the esp32 device
+ * @file    test.cpp
+ * @author  Castox 
+ */
+
+
+/**
+ * @brief   test all possible functions
+ * @return  bool - true if all tests pass
+ */
+bool test_all() {
+    return true;
+}
