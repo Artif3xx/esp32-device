5bee6fa85bc12b3b7a607069e2702f12178cce43
georg <Garbatronprivat@gmail.com>
2022-04-05 16:01:42 +0200

    add docs folder for github documentation page

---
+++ docs/html/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['set_5ftitle_0',['set_title',['../main_8cpp.html#a37af36caa669dcfdbb75813d832bb4dc',1,'main.cpp']]],
+  ['setup_1',['setup',['../main_8cpp.html#a4fc01d736fe50cf5b977f755b675f11d',1,'main.cpp']]]
+];
